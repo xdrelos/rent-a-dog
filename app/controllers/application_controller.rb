d6d53cecc9c990e411cac03646ef4c4f348e3a48
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-09-25 15:15:23 +0200

    devise login using username or email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    attr_user = [:username, :first_name, :last_name, :email, :biography]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attr_user)
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: attr_user)
+
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+  end
 end
