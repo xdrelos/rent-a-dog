622c5402bd9091ecfb7edabc0bf02337a6ba8599
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-09-26 13:14:51 +0200

    add route to create booking and validations on models

--- app/models/renting.rb
+++ app/models/renting.rb
@@ -1,4 +1,6 @@
 class Renting < ApplicationRecord
   belongs_to :user
   belongs_to :dog
+  validates :status, inclusion: { in: %w(Pending Accepted Declined),
+    message: "%{status} is not a valid value" }
 end
