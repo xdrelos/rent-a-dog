34eed4706f7fe11da1d4c233d4e3894d59345787
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-09-27 11:48:45 +0200

    fix

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,7 +2,7 @@ class DogsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    unless params[:query].empty?
+    if params[:id] && !params[:query].empty?
       @pagy, @dogs = pagy(Dog.global_search(params[:query]).order(:palmares), items: 10)
     else
       @pagy, @dogs = pagy(Dog.all.order(:palmares), items: 10)
