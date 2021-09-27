d981caddbb8ea0905467bd5a3d2343674d429413
WomaninTech-spec <barbara.teslar@gmail.com>
2021-09-27 18:10:18 +0200

    création de la  page about

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'rentings/my-rentings', to: 'rentings#my_rentings'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
+  get "about", to: "abouts#about"
 end
