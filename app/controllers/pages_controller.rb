997f5bf9854fb60b98984cf97bc7fed0d0a29bd0
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-09-27 18:55:17 +0200

    authorize about us

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :about]
 
   def home
     @dogs = Dog.all.order('created_at DESC').limit(6)
