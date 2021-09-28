490eb56bce6f45e9a0e72235417e6ef2a36d3b98
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-09-28 19:50:02 +0200

    changes to my rentings

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :dogs do
     resources :rentings, only: [:create, :update]
   end
+  # route for fix the refresh after render a post with errors
+  get 'dogs/:id/rentings', to: redirect('rentings/my-rentings')
   resources :rentings, only: [:edit, :destroy]
   get 'rentings/my-rentings', to: 'rentings#my_rentings'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
