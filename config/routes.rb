dd66eece92525d70ac40396288789492d3fa0629
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-09-30 09:56:30 +0200

    mydogs page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'dogs/my-dogs', to: 'dogs#my_dogs'
   resources :dogs do
     resources :rentings, only: [:create, :update]
   end
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
   get 'dogs/:id/rentings', to: redirect('rentings/my-rentings')
   resources :rentings, only: [:edit, :destroy]
   get 'rentings/my-rentings', to: 'rentings#my_rentings'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "about", to: "pages#about"
