21aeec1f9ee2fdfacb405cfa48580034db81515f
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-10-05 12:33:37 +0200

    add user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: :show
+  skip_after_action :verify_authorized, only: :show
+
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
