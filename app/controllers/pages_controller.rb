f3a9a0f72336bf0c140aedc7a65b6214e97f8bd4
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-10-07 21:25:33 +0200

    rien

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,6 @@ class PagesController < ApplicationController
 
   def home
     @dogs = Dog.all.where.not(user: current_user).order('created_at DESC').limit(6)
-    respond_to do |format|
-      format.html
-      format.json
-    end
   end
 
   def about
